bcb5e5e23766ebb74eb205059bfc6b9cd231cc42
Miyom Dok <miyomdok@gmail.com>
2024-03-07 14:46:15 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+    <head> <title> php variables </title>
+</head>
+<body>
+    <?php
+    //these are some supported variables
+    $x = 5985; // This is integere variables .Notice it is dynamically typed.
+    var_dump($x);
+    $y = 1935;
+    var_dump($y);
+    //some array 
+    $cars = array ("Toyota", "vw","Volvo", "BMW");
+    echo "|prefer" ".$cars[1].",".$cars[0]." "and" ".$cars[3]." ".";
+    ?>
+    </body>
+</html>
